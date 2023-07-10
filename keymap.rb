3f55a7086b6d3eede630b59783f244a0130a390b
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-07-10 15:40:17 +0900

    update keymap.rb

--- keymap.rb
+++ keymap.rb
@@ -15,31 +15,35 @@ kbd.init_pins(
 
 # default layer should be added at first
 kbd.add_layer :default, %i[
-  KC_TAB    KC_Q    KC_W      KC_E     KC_R     KC_T          KC_Y     KC_U     KC_I      KC_O      KC_P KC_BSPACE
-  CTL_ESC   KC_A    KC_S      KC_D     KC_F     KC_G          KC_H     KC_J     KC_K      KC_L KC_SCOLON  KC_QUOTE
+  CTL_ESC    KC_Q    KC_W      KC_E     KC_R     KC_T          KC_Y     KC_U     KC_I      KC_O      KC_P KC_MINUS
+  KC_TAB   KC_A    KC_S      KC_D     KC_F     KC_G          KC_H     KC_J     KC_K      KC_L KC_SCOLON  KC_BSPACE
   KC_LSFT   KC_Z    KC_X      KC_C     KC_V     KC_B          KC_N     KC_M KC_COMMA    KC_DOT  KC_SLASH   KC_RSFT
-  XXXXXXX   XXXXXXX CMD_LANG2 LOWER_NO KC_SPACE KC_SPACE  KC_ENTER KC_ENTER RAISE_NO ALT_LANG1   XXXXXXX   XXXXXXX
+  XXXXXXX   XXXXXXX KC_LCTL  KC_LGUI  KC_SPACE LOWER_KEY       RAISE_KEY  KC_ENTER  SPC_CTL  SPC_CTL   XXXXXXX   XXXXXXX
 ].map { |kc| kc == :XXXXXXX ? :KC_NO : kc }
 kbd.add_layer :raise, %i[
-  KC_TAB  KC_EXLM KC_AT     KC_HASH     KC_DLR   KC_PERC     KC_CIRC  KC_AMPR    KC_ASTER     KC_LPRN KC_RPRN KC_BSPACE
-  CTL_ESC KC_LABK KC_LCBR   KC_LBRACKET KC_LPRN  KC_QUOTE   KC_MINUS KC_EQUAL     KC_LCBR     KC_RCBR KC_PIPE    KC_GRAVE
-  KC_LSFT KC_RABK KC_RCBR   KC_RBRACKET KC_RPRN  KC_DQUO     KC_UNDS  KC_PLUS KC_LBRACKET KC_RBRACKET KC_BSLS   KC_TILD
-  XXXXXXX XXXXXXX CMD_LANG2 LOWER_NO    KC_SPACE KC_SPACE   KC_ENTER KC_ENTER    RAISE_NO   ALT_LANG1 XXXXXXX   XXXXXXX
+  KC_GRAVE  KC_EXLM KC_AT     KC_HASH     KC_DLR   KC_PERC     KC_CIRC  KC_AMPR    KC_ASTER     KC_HASH  KC_AT   KC_EQUAL
+  KC_TAB KC_LABK KC_LCBR   KC_LBRACKET KC_LPRN  KC_QUOTE   KC_LEFT   KC_DOWN   KC_UP     KC_RIGHT KC_UNDS   KC_PIPE
+  KC_LSFT KC_RABK KC_RCBR   KC_RBRACKET KC_RPRN  KC_DQUO     KC_TILD   KC_BSLASH KC_COMMA  KC_DOT   KC_SLASH  KC_RSFT
+  XXXXXXX XXXXXXX KC_LCTL KC_LGUI    KC_SPACE LOWER_KEY   RAISE_KEY  KC_ENTER    RAISE_NO   ALT_LANG1 XXXXXXX   XXXXXXX
 ].map { |kc| kc == :XXXXXXX ? :KC_NO : kc }
 kbd.add_layer :lower, %i[
-  KC_TAB  KC_1    KC_2      KC_3        KC_4      KC_5           KC_6     KC_7     KC_8      KC_9     KC_0 KC_BSPACE
-  CTL_ESC KC_F2   KC_F10    KC_F12      KC_LPRN   KC_QUOTE    KC_LEFT  KC_DOWN    KC_UP  KC_RIGHT KC_RIGHT   XXXXXXX
+  CTL_ESC  KC_1    KC_2      KC_3        KC_4      KC_5           KC_6     KC_7     KC_8      KC_9     KC_0 KC_MINUS
+  KC_TAB KC_F2   KC_F10    KC_F12      KC_LPRN   KC_QUOTE    KC_DOT    KC_4      KC_5      KC_6     KC_PLUS    KC_BSPACE
   KC_LSFT KC_RABK KC_RCBR   KC_RBRACKET KC_RPRN   KC_DQUO        KC_0     KC_1     KC_2      KC_3 KC_SLASH  KC_COMMA
-  XXXXXXX XXXXXXX CMD_LANG2 LOWER_NO    KC_SPACE  KC_SPACE   KC_ENTER KC_ENTER RAISE_NO ALT_LANG1  XXXXXXX   XXXXXXX
+  XXXXXXX XXXXXXX KC_LCTL KC_LGUI    KC_SPACE  LOWER_KEY   RAISE_KEY  KC_ENTER  RAISE_NO ALT_LANG1  XXXXXXX   XXXXXXX
 ].map { |kc| kc == :XXXXXXX ? :KC_NO : kc }
+
+kbd.define_composite_key :SPC_CTL, %i(KC_SPACE KC_RCTL)
 #
 #                   Your custom     Keycode or             Keycode (only modifiers)      Release time      Re-push time
 #                   key name        Array of Keycode       or Layer Symbol to be held    threshold(ms)     threshold(ms)
 #                                   or Proc                or Proc which will run        to consider as    to consider as
 #                                   when you click         while you keep press          `click the key`   `hold the key`
-kbd.define_mode_key :CTL_ESC,     [ :KC_ESCAPE,            :KC_LCTL,                     120,              150 ]
-kbd.define_mode_key :RAISE_NO,    [ :KC_NO,                :raise,                       120,              150 ]
-kbd.define_mode_key :LOWER_NO,    [ :KC_NO,                :lower,                       120,              400 ]
+kbd.define_mode_key :CTL_ESC,     [ :KC_ESCAPE,            :KC_LCTL,                     200,              200 ]
+kbd.define_mode_key :RAISE_ENT,    [ :KC_ENTER,                :raise,                       200,              200 ]
+kbd.define_mode_key :LOWER_SPC,    [ :KC_SPACE,                :lower,                       400,              200 ]
+kbd.define_mode_key :RAISE_KEY,    [ nil,                :raise,                       nil,               nil ]
+kbd.define_mode_key :LOWER_KEY,    [ nil,                :lower,                       nil,              nil ]
 kbd.define_mode_key :ALT_LANG1,   [ :KC_LANG1,             :KC_LALT,                     120,              400 ]
 kbd.define_mode_key :CMD_LANG2,   [ :KC_LANG2,             :KC_RGUI,                     120,              400 ]
 
@@ -47,8 +51,8 @@ kbd.define_mode_key :CMD_LANG2,   [ :KC_LANG2,             :KC_RGUI,            
 # You can use it to hack data by adding an instance method to Keyboard class by yourself.
 # ex) Use Keyboard#before_report filter if you want to input `":" w/o shift` and `";" w/ shift`
 kbd.before_report do
- kbd.invert_sft if kbd.keys_include?(:KC_SCOLON)
- # You'll be also able to write `invert_ctl`, `invert_alt` and `invert_gui`
+  kbd.invert_sft if kbd.keys_include?(:KC_SCOLON)
+  # You'll be also able to write `invert_ctl`, `invert_alt` and `invert_gui`
 end
 
 # Initialize RGBLED with pin, underglow_size, backlight_size and is_rgbw.
@@ -61,20 +65,7 @@ rgb = RGB.new(
 # Set an effect
 #  `nil` or `:off` for turning off, `:breathing` for "color breathing", `:rainbow` for "rainbow snaking"
 
-RGB_COL_COUNT = 12
-RGB_ROW_COUNT = 4
-
-[
-  [169, 10], [135,  6], [101,  2], [67,   6], [ 33, 12], [  0, 20],
-  [  0, 34], [ 33, 27], [ 67, 20], [101, 17], [135, 20], [169, 24],
-  [169, 38], [135, 34], [101, 31], [67,  34], [ 33, 41], [  0, 48],
-  [135, 55], [159, 61], [194, 57], [223, 64],
-  [223, 38], [194, 31], [194,  2], [223,  2]
-].each do |p|
-  rgb.add_pixel(p[0],p[1])
-end
-
-rgb.effect = :circle
+rgb.effect = :swirl
 rgb.speed = 22
 # rgb.effect = :breathing
 # Set an action when you input
